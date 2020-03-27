688044722d4384888d5c7734b72bd5f3d6ba7624
Leonardo Mayer Kleesattel <leonardo.kleesattel@jv01.local>
2020-03-27 00:25:03 -0300

    Backend application

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const ong = await connection('ong').select('*');
+        return response.json(ong);
+    },
+    
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ong').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
+
+        return response.json(id);
+    }
+};
